7e0048b83e0ec21936942c33d08fd05a516144f5
JustinElMas <justin7b.jjr2014@gmail.com>
2023-07-14 18:37:52 -0500

    Add the new files and folders

---
+++ src/main/java/justin/com/example/demogitconjava/UsuarioModel.java
@@ -0,0 +1,7 @@
+package justin.com.example.demogitconjava;
+
+public class UsuarioModel {
+    
+    
+
+}
---
+++ src/main/java/justin/com/example/demogitconjava/messages/MensajeController.java
@@ -0,0 +1,7 @@
+package justin.com.example.demogitconjava.messages;
+
+public class MensajeController {
+    
+    
+
+}
