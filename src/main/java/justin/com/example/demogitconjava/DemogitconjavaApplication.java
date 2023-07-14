615dab4b39c4f9fbd4239cdf83a50b1a7a8cf41d
JustinElMas <justin7b.jjr2014@gmail.com>
2023-07-14 17:31:11 -0500

    Add all files and folders

---
+++ src/main/java/justin/com/example/demogitconjava/DemogitconjavaApplication.java
@@ -0,0 +1,13 @@
+package justin.com.example.demogitconjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitconjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitconjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/justin/com/example/demogitconjava/DemogitconjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package justin.com.example.demogitconjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitconjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
